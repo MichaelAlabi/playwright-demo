35ba5632c42196e64d1f58fec050d5f8439ba591
MichaelAlabi <michaelalabi82@gmail.com>
2026-04-18 16:06:37 +0100

    updated tests

--- tests/invalidLogin.spec.ts
+++ tests/invalidLogin.spec.ts
@@ -2,7 +2,7 @@ import { test, expect} from "@playwright/test";
 import { LoginPage } from "./login.js";
 import dotenv from "dotenv";
 
-test("validate that user can not login with invalid credentials", async ({ page }) => {
+test("verify that user cannot sign in with invalid credentials", async ({ page }) => {
   const loginPage = new LoginPage(page as any);
   await loginPage.goto();
   await loginPage.login(
