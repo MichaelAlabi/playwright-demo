f4b391426dfea4d8ab150d7252a14a22b8f1d3ea
MichaelAlabi <michaelalabi82@gmail.com>
2026-04-18 17:28:36 +0100

    changed spec name

---
+++ tests/validLogin.spec.ts
@@ -0,0 +1,12 @@
+import { test} from "@playwright/test";
+import { LoginPage } from "./login.js";
+import dotenv from "dotenv";
+
+test("verify that user can successfully sign in", async ({ page }) => {
+  const loginPage = new LoginPage(page as any);
+  await loginPage.goto();
+  await loginPage.login(
+    process.env.user1_E2E_USERNAME!,
+    process.env.user1_E2E_PASSWORD!,
+  );
+});
